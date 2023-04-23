c0215e098bf1910c7aa486a832a48e8776a0d907
kaif47 <74183862+kaif47@users.noreply.github.com>
2023-04-23 22:25:14 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,1 +1,17 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+print("sample test case started")
+driver = webdriver.Chrome(r"C:\Users\Kaif Bijali\PycharmProjects\seleniumtest2\Browsers\chromedriver.exe")
 
+driver.maximize_window()
+driver.get("https://www.google.com/")
+kaif = driver.find_element("name","q")
+kaif.send_keys("Lord Rinku Singh")
+time.sleep(3)
+kaif = driver.find_element("name","btnK")
+kaif.send_keys(Keys.ENTER)
+time.sleep(3)
+#close the browser
+driver.close()
+print("sample test case successfully completed")
